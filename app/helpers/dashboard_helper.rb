b9ee752e9e5278c9dface5c296d886f212bfeb51
martent <marten@thavenius.se>
2013-05-07 11:24:39 +0200

    Text helper. Style fix

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 module DashboardHelper
   def summary(text)
     sanitize truncate( strip_tags(text), { separator: ' ', length: 140, omission: ' …' } )
@@ -8,5 +7,10 @@ module DashboardHelper
   # Is the feed entry a tweet?
   def tweet?(entry)
     entry.guid.present? && !!entry.guid.match('^http://twitter.com/')
+  end
+
+  # Add "s" to the end of a name if not already there. Swedish, no apostrophe.
+  def possessive(owner)
+    owner.match(/[sz]$/) ? owner : "#{owner}s"
   end
 end
