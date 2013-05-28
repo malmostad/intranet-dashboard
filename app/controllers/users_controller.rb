b7bfed43d3cb00561aac984adcefab3e7da43006
martent <marten@thavenius.se>
2013-05-28 09:54:50 +0200

    Edit in place template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,9 @@ class UsersController < ApplicationController
     @user = User.where(id: params[:id]).includes(:roles).first
     @user_roles = user_roles
     @roles = Role.all
+    if params["mode"] == "edit_in_place"
+      render layout: false
+    end
   end
 
   def update
