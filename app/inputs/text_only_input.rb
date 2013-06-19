0966a69c7ab82669bbc20fc727aab79735541656
martent <marten@thavenius.se>
2013-06-19 19:16:45 +0200

    Moved custom inputs to initializers

--- app/inputs/text_only_input.rb
+++
@@ -1,6 +0,0 @@
-class TextOnlyInput < SimpleForm::Inputs::Base
-  # Return the attribute as a string
-  def input
-    self.object.send(attribute_name.to_sym).to_s
-  end
-end
--- app/inputs/text_only_more_info_input.rb
+++
@@ -1,6 +0,0 @@
-class TextOnlyMoreInfoInput < SimpleForm::Inputs::TextOnlyInput
-  # Return the attribute as a string + an <a> tag used by javascript to show more info
-  def input
-    super + template.content_tag(:a, "(info)", href: "#", class: "show-more-info")
-  end
-end
