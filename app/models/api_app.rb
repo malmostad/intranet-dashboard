02dc2c00d13624b241d5364bf5b771be89ae55ef
martent <marten@thavenius.se>
2013-10-06 17:19:55 +0200

    API auth

--- app/models/api_app.rb
+++ app/models/api_app.rb
@@ -1,11 +1,17 @@
 class ApiApp < ActiveRecord::Base
   attr_accessible :contact, :ip_address, :name
+  attr_reader :auth_message
 
   has_secure_password
   alias_attribute :app_secret, :password # because `app_secret` is a better name in the api
 
   before_validation :generate_app_token, :generate_app_secret, on: :create
   validates_presence_of :name, :contact, :ip_address
+
+  def self.authenticate(app_token, app_secret, ip_address)
+    app = where(app_token: app_token, ip_address: ip_address).first
+    app && app.authenticate(app_secret)
+  end
 
   def generate_app_secret
     self.password = SecureRandom.hex
