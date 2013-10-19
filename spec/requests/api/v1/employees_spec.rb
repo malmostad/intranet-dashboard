11124e205fbb1aa947e8c1fbfcd2c5ed3a0f12d2
martent <marten@thavenius.se>
2013-10-19 17:10:44 +0200

    Employees API specs

--- spec/requests/api/v1/employees_spec.rb
+++ spec/requests/api/v1/employees_spec.rb
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
-require 'spec_helper'
+require "spec_helper"
 
 describe "Employees API" do
   it "should require authentication" do
-    get '/api/v1/employees/123'
+    get "/api/v1/employees/123"
+    expect(response.status).to eq(401)
+    get "/api/v1/employees/search"
     expect(response.status).to eq(401)
   end
 
   it "should have an unauthorized message" do
-    get '/api/v1/employees/123'
-    expect(json['message']).to eq("401 Unauthorized. Your app_token, app_secret or ip address is not correct")
+    get "/api/v1/employees/123"
+    expect(json["message"]).to eq("401 Unauthorized. Your app_token, app_secret or ip address is not correct")
+  end
+
+  it "should ..." do
+    # get "/api/v1/employees/#{user.username}?app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
   end
 end
