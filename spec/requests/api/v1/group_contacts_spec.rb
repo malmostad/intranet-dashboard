2c4c0c4f4eab7ea4c71d0d40252b765d7d6bb75b
martent <marten@thavenius.se>
2013-10-22 22:00:53 +0200

    Specs for group contacts

--- spec/requests/api/v1/group_contacts_spec.rb
+++ spec/requests/api/v1/group_contacts_spec.rb
@@ -45,169 +45,159 @@ describe "GroupContactsAPI" do
 
   describe "search response" do
     it "should be a success" do
-      get "/api/v1/group_contacts/search?term=#{group_contact.id}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+      get "/api/v1/group_contacts/search?term=#{group_contact.name}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
       expect(response).to be_success
     end
 
-  #   it "should contain one employee" do
-  #     get "/api/v1/group_contacts/search?term=#{group_contact.id}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
-  #     expect(json.size).to eq(1)
-  #   end
+    it "should contain one group_contact" do
+      get "/api/v1/group_contacts/search?term=#{group_contact.name}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+      expect(json.size).to eq(1)
+    end
 
-  #   it "should not contain any employee" do
-  #     get "/api/v1/group_contacts/search?term=foo&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
-  #     expect(json.size).to eq(0)
-  #   end
+    it "should not contain any group_contact" do
+      get "/api/v1/group_contacts/search?term=foo&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+      expect(json.size).to eq(0)
+    end
 
-  #   it "should contain two group_contacts" do
-  #     create(:user, username: "foo-1")
-  #     create(:user, username: "foo-2")
-  #     get "/api/v1/group_contacts/search?term=foo-&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
-  #     expect(json.size).to eq(2)
-  #   end
+    it "should contain two group_contacts" do
+      create(:group_contact, name: "foo-1")
+      create(:group_contact, name: "foo-2")
+      get "/api/v1/group_contacts/search?term=foo-&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+      expect(json.size).to eq(2)
+    end
 
-  #   describe "first matching employee" do
-  #     before(:each) do
-  #       get "/api/v1/group_contacts/search?term=#{group_contact.id}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
-  #     end
+    describe "first matching group_contact" do
+      before(:each) do
+        get "/api/v1/group_contacts/search?term=#{group_contact.name}&app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+      end
 
-  #     it "should have an id" do
-  #       expect(json.first["id"]).to eq(group_contact.id)
-  #     end
+      it "should have an id" do
+        expect(json.first["id"]).to eq(group_contact.id)
+      end
 
-  #     it "should have a first_name" do
-  #       expect(json.first["firstName"]).to eq(group_contact.first_name)
-  #     end
+      it "should have a name" do
+        expect(json.first["name"]).to eq(group_contact.name)
+      end
 
-  #     it "should have an last_name" do
-  #       expect(json.first["lastName"]).to eq(group_contact.last_name)
-  #     end
+      it "should not give away all attributes" do
+        %w(
+          address
+          cellPhone
+          email
+          fax
+          homepage
+          phone
+          phoneHours
+          postalTtown
+          visitingHours
+          visitorsAddress
+          visitorsAddressGeoPositionX
+          visitorsAddressGeoPositionY
+          visitorsAddressPostalTown
+          visitorsAddressZipCode
+          visitorsDistrict
+          zipCode
+          createdAt
+          updatedAt
+        ).each do |attribute|
+          expect(json.first[attribute]).to eq(nil)
+        end
+      end
+    end
+  end
 
-  #     it "should have a title" do
-  #       expect(json.first["title"]).to eq(group_contact.title)
-  #     end
+  describe "response" do
+    before(:each) do
+      get "/api/v1/group_contacts/#{group_contact.id}?app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
+    end
 
-  #     it "should have an email" do
-  #       expect(json.first["email"]).to eq(group_contact.email)
-  #     end
+    it "should be a success" do
+      expect(response).to be_success
+    end
 
-  #     it "should have a company" do
-  #       expect(json.first["company"]).to eq(group_contact.company)
-  #     end
+    it "should return the id" do
+      expect(json["id"]).to eq(group_contact.id)
+    end
 
-  #     it "should have a department " do
-  #       expect(json.first["department"]).to eq(group_contact.department)
-  #     end
+    it "should return the name" do
+      expect(json["name"]).to eq(group_contact.name)
+    end
 
-  #     it "should not give away private attributes" do
-  #       %w(
-  #         statusMessage
-  #         admin
-  #         createdAt
-  #         updatedAt
-  #         lastLogin
-  #         statusMessageUpdatedAt
-  #         earlyAdopter
-  #         twitter
-  #         skype
-  #         privateBio
-  #         managerId
-  #         homepage
-  #         cmgId
-  #         deactivated
-  #         deactivatedAt
-  #       ).each do |attribute|
-  #         expect(json.first[attribute]).to eq(nil)
-  #       end
-  #     end
-  #   end
-  # end
+    it "should return email" do
+      expect(json["email"]).to eq(group_contact.email)
+    end
 
-  # describe "response" do
-  #   before(:each) do
-  #     get "/api/v1/group_contacts/#{group_contact.id}?app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
-  #   end
+    it "should return phone" do
+      expect(json["phone"]).to eq(group_contact.phone)
+    end
 
-  #   it "should be a success" do
-  #     expect(response).to be_success
-  #   end
+    it "should return phone_hours" do
+      expect(json["phoneHours"]).to eq(group_contact.phone)
+    end
 
-  #   it "should return employee id" do
-  #     expect(json["id"]).to eq(group_contact.id)
-  #   end
+    it "should return cell_phone" do
+      expect(json["cellPhone"]).to eq(group_contact.cell_phone)
+    end
 
-  #   it "should return employee catalogId" do
-  #     expect(json["catalogId"]).to eq(group_contact.id)
-  #   end
+    it "should return fax" do
+      expect(json["fax"]).to eq(group_contact.fax)
+    end
 
-  #   it "should return employee firstName" do
-  #     expect(json["firstName"]).to eq(group_contact.first_name)
-  #   end
+    it "should return address" do
+      expect(json["address"]).to eq(group_contact.address)
+    end
 
-  #   it "should return employee catalogId" do
-  #     expect(json["lastName"]).to eq(group_contact.last_name)
-  #   end
+    it "should return zip_code" do
+      expect(json["zipCode"]).to eq(group_contact.zip_code)
+    end
 
-  #   it "should return employee title" do
-  #     expect(json["title"]).to eq(group_contact.title)
-  #   end
+    it "should return postal_town" do
+      expect(json["postalTown"]).to eq(group_contact.postal_town)
+    end
 
-  #   it "should return employee email" do
-  #     expect(json["email"]).to eq(group_contact.email)
-  #   end
+    it "should return visitors_address" do
+      expect(json["visitorsAddress"]).to eq(group_contact.visitors_address)
+    end
 
-  #   it "should return employee phone" do
-  #     expect(json["phone"]).to eq(group_contact.phone)
-  #   end
+    it "should return visitors_address_zip_code" do
+      expect(json["visitorsAddressZipCode"]).to eq(group_contact.visitors_address_zip_code)
+    end
 
-  #   it "should return employee cellPhone" do
-  #     expect(json["cellPhone"]).to eq(group_contact.cell_phone)
-  #   end
+    it "should return visitors_address_postal_town" do
+      expect(json["visitorsAddressPostalTown"]).to eq(group_contact.visitors_address_postal_town)
+    end
 
-  #   it "should return employee company" do
-  #     expect(json["company"]).to eq(group_contact.company)
-  #   end
+    it "should return visitors_district" do
+      expect(json["visitorsDistrict"]).to eq(group_contact.visitors_district)
+    end
 
-  #   it "should return employee department" do
-  #     expect(json["department"]).to eq(group_contact.department)
-  #   end
+    it "should return visitors_address_geo_position_x" do
+      expect(json["visitorsAddressGeoPositionX"]).to eq(group_contact.visitors_address_geo_position_x)
+    end
 
-  #   it "should return employee address" do
-  #     expect(json["address"]).to eq(group_contact.address)
-  #   end
+    it "should return visitors_address_geo_position_y" do
+      expect(json["visitorsAaddressGeoPositionY"]).to eq(group_contact.visitors_address_geo_position_y)
+    end
 
-  #   it "should return employee room" do
-  #     expect(json["room"]).to eq(group_contact.room)
-  #   end
+    it "should return visiting_hours" do
+      expect(json["visitingHours"]).to eq(group_contact.visiting_hours)
+    end
 
-  #   it "should return employee roles array" do
-  #     expect(json["roles"].class).to eq(Array)
-  #   end
+    it "should return homepage" do
+      expect(json["homepage"]).to eq(group_contact.homepage)
+    end
 
-  #   it "should not give away private attributes" do
-  #     %w(
-  #       statusMessage
-  #       admin
-  #       createdAt
-  #       updatedAt
-  #       lastLogin
-  #       statusMessageUpdatedAt
-  #       earlyAdopter
-  #       twitter
-  #       skype
-  #       privateBio
-  #       managerId
-  #       homepage
-  #       cmgId
-  #       deactivated
-  #       deactivatedAt
-  #     ).each do |attribute|
-  #       expect(json[attribute]).to eq(nil)
-  #     end
-  #   end
-  # end
+    it "should not give away private attributes" do
+      %w(
+        createdAt
+        updatedAt
+      ).each do |attribute|
+        expect(json[attribute]).to eq(nil)
+      end
+    end
+  end
 
-  # it "should return response by user id" do
+  # it "should return response by group_contact id" do
   #   get "/api/v1/group_contacts/#{group_contact.id}?app_token=#{api_app.app_token}&app_secret=#{api_app.app_secret}"
   #   pending("implement feature")
   #   # expect(response).to be_success
@@ -215,5 +205,5 @@ describe "GroupContactsAPI" do
   #   # expect(json["catalogId"]).to eq(group_contact.id)
   #   # expect(json["firstName"]).to eq(group_contact.first_name)
   #   # expect(json["lastName"]).to eq(group_contact.last_name)
-  end
+#  end
 end
