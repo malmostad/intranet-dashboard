5c4355e5f52b8947777b73e3cc5392361c8ea5c5
martent <marten@thavenius.se>
2013-12-19 21:16:00 +0100

    Added users without address' and  diff to statistics menu

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -9,6 +9,7 @@ class StatisticsController < ApplicationController
     @user_stats["total_users"] = User.count
     @user_stats["last_week_users"] = User.where("last_login > ?", Time.now - 1.week).count
     @user_stats["registered_last_week_users"] = User.where("created_at > ?", Time.now - 1.week).count
+    @user_stats["has_address"] = @user_stats["total_users"] - User.where(address: [nil, ""]).count
     @user_stats["has_status"] = User.where("status_message != ?", "").count
     @user_stats["has_avatar"] = User.where("avatar_updated_at != ?", "").count
     @user_stats
