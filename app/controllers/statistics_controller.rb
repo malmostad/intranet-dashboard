1500f09448f6db32c72fb274e3f78f08d02c0d06
martent <marten@thavenius.se>
2013-12-19 20:53:33 +0100

    Added statistics file download

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,17 +5,9 @@ class StatisticsController < ApplicationController
   before_filter :require_admin
 
   def index
-    render text: "index"
   end
 
   def ldap_diff
-    render text: "ldap diff"
-    # headers['Content-Disposition'] = 'attachment; filename="ldap_diff-' + Time.now().to_i.to_s + '.xls"'
-    # headers['Cache-Control'] = ''
-    # respond_to do |format|
-    #   format.xls { render layout: false, content_type: "application/vnd.ms-excel" }
-    # end
+    send_file File.join(Rails.root, "data", "ldap_diff.xls"), type: :xls, disposition: "attachment", filename: "ldap_diff.xls"
   end
 end
-
-
