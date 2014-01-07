ff4568fc760269ed5d210e065be7226715582428
martent <marten@thavenius.se>
2014-01-07 11:16:06 +0100

    Validation for locals

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Dashboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = "sv-SE"
+    I18n.config.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
