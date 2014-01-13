19d0d44c91e1948f5818f1f163a7e2e8d7374954
martent <marten@thavenius.se>
2014-01-13 17:51:19 +0100

    Skeleton for test case for merge languages

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -27,4 +27,33 @@ describe Language do
     language = create(:language)
     expect { language.destroy }.to change(Language, :count).by(-1)
   end
+
+  describe "merged into another" do
+    before(:each) do
+      @language_1 = create(:language)
+      @user_1 = create(:user, languages: [@language_1])
+      @language_2 = create(:language)
+      @user_2 = create(:user, languages: [@language_2])
+    end
+
+    it "should destroy the first language" do
+      puts Language.count
+      puts Language.first
+      puts User.count
+      puts @user_1
+      puts @user_1.languages.first.name
+      puts @language_1.users.first.username
+      # expect { Language.merge(Language.first.id, Language.limit(2).last.id) }.to change(Language, :count).by(-1)
+    end
+
+    it "should not destroy the second language" do
+    end
+
+    it "should not change users number of languages" do
+      # expect { Language.merge(Language.first.id, Language.limit(2).last.id) }.to change(user_1.languages, :count).by(0)
+    end
+
+    it "should not change users number of languages when she has both the old and the new language" do
+    end
+  end
 end
