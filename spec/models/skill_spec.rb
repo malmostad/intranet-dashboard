6451a3e7b0dfea15987b486335357ff2ab06c1a2
martent <marten@thavenius.se>
2014-01-15 17:01:13 +0100

    Test cases for merging skills

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -27,4 +27,31 @@ describe Skill do
     skill = create(:skill)
     expect { skill.destroy }.to change(Skill, :count).by(-1)
   end
+
+  describe "merged" do
+    before(:each) do
+      @skill_1 = create(:skill)
+      @user_1 = create(:user, skills: [@skill_1])
+      @skill_2 = create(:skill)
+      @user_2 = create(:user, skills: [@skill_2])
+    end
+
+    it "should destroy the first language" do
+      expect { @skill_1.merge(@skill_2) }.to change(Language, :count).by(-1)
+    end
+
+    it "should not destroy the second language" do
+      @skill_1.merge(@skill_2)
+      expect(Language.last).to eq(@skill_2)
+    end
+
+    it "should not change a users number of skills (if she don't have both)" do
+      expect { @skill_1.merge(@skill_2) }.to change(@user_1.skills, :count).by(0)
+    end
+
+    it "should change a users number of skills (if she has both)" do
+      @user_1.skills << @skill_2
+      expect { @skill_1.merge(@skill_2) }.to change(@user_1.skills, :count).by(-1)
+    end
+  end
 end
