a462523381330799012ebca2d640943dc6b0e940
martent <marten@thavenius.se>
2014-01-22 13:28:22 +0100

    Added number_to_words localized helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,11 @@ module ApplicationHelper
     separator
   end
 
+  def number_to_words(number, alt = false)
+    words = alt ? I18n.t("number_words_alt") : I18n.t("number_words")
+    words[number] ? words[number] : number.to_s
+  end
+
   def page_title
     !@title.nil? ? "#{@title} - #{title_suffix}" : title_suffix
   end
