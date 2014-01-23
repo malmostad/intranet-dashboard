9dca12cbf4bd9a2763b582340857227cdb508e1f
martent <marten@thavenius.se>
2014-01-23 16:18:06 +0100

    Test cases for stub authentication

--- spec/features/authentication_ldap_spec.rb
+++ spec/features/authentication_ldap_spec.rb
@@ -3,44 +3,6 @@ require 'spec_helper'
 
 if APP_CONFIG["auth_method"] == "ldap"
   describe "LDAP authentication" do
-    it "should require login for the dashboard" do
-      visit root_path
-      current_path.should eq(login_path)
-      page.should have_selector('h1', text: "Logga in")
-      page.should have_field("password")
-    end
-
-    it "should require login for profile page" do
-      user = create(:user)
-      visit user_path(user.username)
-      current_path.should eq(login_path)
-      page.should have_selector('h1', text: "Logga in")
-    end
-
-    it "should require login for an admin page" do
-      visit roles_path
-      current_path.should eq(login_path)
-      page.should have_selector('h1', text: "Logga in")
-    end
-
-    it "should not sign in a user with incorrect credentials" do
-      visit login_path
-      fill_in 'username', with: 'Fox'
-      fill_in 'password', with: 'barx'
-      click_button 'Logga in'
-      current_path.should eq(login_path)
-      page.should have_selector('.warning', text: 'Fel användarnamn eller lösenord')
-    end
-
-    it "should not sign in a user without credentials" do
-      visit login_path
-      fill_in 'username', with: ''
-      fill_in 'password', with: ''
-      click_button 'Logga in'
-      current_path.should eq(login_path)
-      page.should have_selector('.warning', text: 'Fel användarnamn eller lösenord')
-    end
-
     it "should sign in a user with correct credentials" do
       create_ldap_user
       login_ldap_user
@@ -61,26 +23,6 @@ if APP_CONFIG["auth_method"] == "ldap"
       login_ldap_user
       visit feeds_path
       page.should have_selector('h1', text: "Nyhetsflöden")
-    end
-
-    it "should redirect to requested page after login" do
-      user = create(:user)
-      visit user_path(user.username)
-      current_path.should eq(login_path)
-      fill_in 'username', with: AUTH_CREDENTIALS["username"]
-      fill_in 'password', with: AUTH_CREDENTIALS["password"]
-      click_button 'Logga in'
-      current_path.should eq(user_path(user.username))
-    end
-
-    it "should not register ajax request as request to redirect after login", js: true do
-      user = create(:user)
-      visit user_path(user.username)
-      page.execute_script "$.get('#{users_path}')"
-      fill_in 'username', with: AUTH_CREDENTIALS["username"]
-      fill_in 'password', with: AUTH_CREDENTIALS["password"]
-      click_button 'Logga in'
-      current_path.should eq(user_path(user.username))
     end
   end
 end
---
+++ spec/features/authentication_stub_spec.rb
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe " Authentication (stub)" do
+  it "should require login for the dashboard" do
+    visit root_path
+    current_path.should eq(login_path)
+    page.should have_selector('h1', text: "Logga in")
+    page.should have_field("password")
+  end
+
+  it "should require login for profile page" do
+    user = create(:user)
+    visit user_path(user.username)
+    current_path.should eq(login_path)
+    page.should have_selector('h1', text: "Logga in")
+  end
+
+  it "should require login for an admin page" do
+    visit roles_path
+    current_path.should eq(login_path)
+    page.should have_selector('h1', text: "Logga in")
+  end
+
+  it "should not sign in a user with incorrect credentials" do
+    visit login_path
+    fill_in 'username', with: 'Fox'
+    fill_in 'password', with: 'barx'
+    click_button 'Logga in'
+    current_path.should eq(login_path)
+    page.should have_selector('.warning', text: 'Fel användarnamn eller lösenord')
+  end
+
+  it "should not sign in a user without credentials" do
+    visit login_path
+    fill_in 'username', with: ''
+    fill_in 'password', with: ''
+    click_button 'Logga in'
+    current_path.should eq(login_path)
+    page.should have_selector('.warning', text: 'Fel användarnamn eller lösenord')
+  end
+
+  it "should redirect to requested page after login" do
+    user = create(:user)
+    visit user_path(user.username)
+    current_path.should eq(login_path)
+    fill_in 'username', with: user.username
+    fill_in 'password', with: "bar"
+    click_button 'Logga in'
+    current_path.should eq(user_path(user.username))
+  end
+
+  it "should not register ajax request as request to redirect after login", js: true do
+    user = create(:user)
+    visit user_path(user.username)
+    page.execute_script "$.get('#{users_path}')"
+    fill_in 'username', with: user.username
+    fill_in 'password', with: "bar"
+    click_button 'Logga in'
+    current_path.should eq(user_path(user.username))
+  end
+end
