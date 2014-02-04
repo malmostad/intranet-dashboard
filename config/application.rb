7aa2f68d5b423710591e06312e77bf5731ed461b
martent <marten@thavenius.se>
2014-02-04 12:49:34 +0100

    Refactoring: extracted Elastic stuff to a concern

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Dashboard
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{Rails.root}/app/services)
+    config.autoload_paths += %W(#{Rails.root}/app/models/concerns)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
