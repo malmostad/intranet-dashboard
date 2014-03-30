59a8b5197e9f1aaa8a160461bfb468263f0a000e
martent <marten@thavenius.se>
2014-03-30 15:27:41 +0200

    Removed jobs for clearing sessions

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,10 +17,6 @@ if environment == "staging"
   # every :day, :at => '2:13am' do
   #   rake "delete_old_feed_entries"
   # end
-
-  # every :day, :at => '2:43am' do
-  #   rake "users:clear_expired_sessions"
-  # end
 end
 
 if environment == "production"
@@ -31,15 +27,11 @@ if environment == "production"
   every :day, :at => '4:13am' do
     rake "delete_old_feed_entries"
   end
-
-  every :day, :at => '4:43am' do
-    rake "users:clear_expired_sessions"
-  end
 end
 
 every :reboot do
   if environment == "production"
-    command "sleep 60; RAILS_ENV=#{real_environment} #{path}/lib/daemons/feed_worker_ctl start"
-    command "sleep 60; RAILS_ENV=#{real_environment} #{path}/script/delayed_job start"
+    command "sleep 120; RAILS_ENV=#{real_environment} #{path}/lib/daemons/feed_worker_ctl start"
+    command "sleep 120; RAILS_ENV=#{real_environment} #{path}/script/delayed_job start"
   end
 end
--- lib/tasks/users.rake
+++ lib/tasks/users.rake
@@ -78,7 +78,6 @@ namespace :users do
   task clear_expired_sessions: :environment do
     t = Time.new.to_f
     ActiveRecord::Base.connection.execute('DELETE FROM sessions WHERE updated_at < DATE_SUB(NOW(), INTERVAL 12 HOUR);')
-    # ActiveRecord::Base.connection.execute('OPTIMIZE TABLE sessions;')
     puts "#{Time.now} Expired sessions removed in #{(Time.new.to_f - t)} seconds"
   end
 end
