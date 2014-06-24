32bb24608a801162ab485cd0ca8613ae816d1764
martent <marten@thavenius.se>
2014-06-24 12:00:19 +0200

    Touch friendly links #41

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,22 +10,4 @@ module UsersHelper
   def load_more_query
     { page: params[:page].to_i + 1 }.merge(params.except(:controller, :action, :page))
   end
-
-  def skill_link_list(skills)
-    skills.map do |skill|
-      link_to skill.name, users_tags_path(skill: skill.name)
-    end.join(", ")
-  end
-
-  def activity_link_list(activities)
-    activities.map do |activity|
-      link_to activity.name, users_tags_path(activity: activity.name)
-    end.join(", ")
-  end
-
-  def language_link_list(languages)
-    languages.map do |language|
-      link_to language.name, users_tags_path(language: language.name)
-    end.to_sentence.humanize
-  end
 end
