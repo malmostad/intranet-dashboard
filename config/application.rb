4fdb80ac1f17153a5e382220f1008a93936594e5
martent <marten@thavenius.se>
2014-08-25 13:24:36 +0200

    Bug fix

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, Rails.env)
 
 module Dashboard
   class Application < Rails::Application
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
