43d6438fec0409328489e7c15236b8337b088685
martent <marten@thavenius.se>
2014-08-26 13:41:57 +0200

    Added pid to logging

--- config/initializers/log_formatter.rb
+++ config/initializers/log_formatter.rb
@@ -1,5 +1,5 @@
 class ActiveSupport::Logger::SimpleFormatter
   def call(severity, time, progname, msg)
-    "#{time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)} [#{severity}] #{msg}\n"
+    "#{time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)} [#{severity}] [#{$$}] #{msg}\n"
   end
 end
