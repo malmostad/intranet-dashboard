26d28950ee7cf96143a44b0b01e0e7b00ed700be
martent <marten@thavenius.se>
2014-09-01 11:15:50 +0200

    Removed Rails 3's secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dashboard::Application.config.secret_token = APP_CONFIG["secret_token"]
 Dashboard::Application.config.secret_key_base = APP_CONFIG["secret_key_base"]
