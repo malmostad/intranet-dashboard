b7850257eaa97da9951686e7457ac319d189cbdc
martent <marten@thavenius.se>
2015-01-08 16:03:13 +0100

    Config for deprecation warnings in Rails 4.2

--- config/initializers/simple_form_components.rb
+++ config/initializers/simple_form_components.rb
@@ -3,14 +3,14 @@ module SimpleForm
 
     # Return the attribute as a string
     class TextOnlyInput < Base
-      def input
+      def input(wrapper_options)
         self.object.send(attribute_name.to_sym).to_s
       end
     end
 
     # Return the attribute as a string + an <a> tag used by javascript to show more info
     class MoreInfoInput < TextOnlyInput
-      def input
+      def input(wrapper_options)
         super + template.content_tag(:a, " (info)", href: "#", class: "show-more-info")
       end
     end
