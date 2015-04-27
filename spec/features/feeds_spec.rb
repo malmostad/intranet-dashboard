ecf2eadf2f1f8d88acc318542efe30cc23a65adc
martent <marten@thavenius.se>
2015-04-27 11:03:35 +0200

    Update feed specs

--- spec/features/feeds_spec.rb
+++ spec/features/feeds_spec.rb
@@ -16,46 +16,16 @@ describe "Feeds" do
   end
 
   it "should have news feed entries" do
-    expect(all("section.news .box-content li").count).to be > 1
-  end
-
-  it "should have dialog feed entries" do
-    expect(all("section.dialog .box-content li").count).to be > 1
-  end
-
-  it "should have one feature feed entry" do
-    expect(all("#feeds-feature .box-content li").count).to eq 1
-  end
-
-  it "should load more news feed entries", js: true do
-    before = all("section.news .box-content li").count
-    expect(find("section.news .box-content li.load-more button").text).to eq "Visa fler nyheter"
-    find("section.news .box-content li.load-more button").click
-    expect(find("section.news .box-content li.load-more button").text).to eq "Hämtar fler..."
-    sleep 1
-    expect(before).to be < all("section.news .box-content li").count
-  end
-
-  it "should switch to combined news and back again", js: true do
-    first(".box.feeds .dropdown button").click
-    click_link("Visa sammanslaget")
-    expect(page).to_not have_selector('section.news h1', text: "Nyheter")
-    expect(@user.reload.combined_feed_stream?).to eq true
-    first(".box.feeds .dropdown button").click
-    click_link("Visa kategoriserat")
-    expect(page).to have_selector('section.news h1', text: "Nyheter")
-    expect(@user.reload.combined_feed_stream?).to be false
+    expect(all("section.feeds .box-content li").count).to be > 1
   end
 
   it "should lazy load more combined news feed entries", js: true do
-    first(".box.feeds .dropdown button").click
-    click_link("Visa sammanslaget")
-    before = all(".combined .box-content li").count
+    before = all("section.feeds .box-content li").count
     expect(find("li.load-more button").text).to eq "Visa fler"
     page.execute_script("window.scrollTo(0, 10000)")
-    expect(find(".box-content li.load-more button").text).to eq "Hämtar fler..."
+    expect(find(".box-content li.load-more button").text).to include 'Visa fler'
     sleep 0.2
-    expect(before).to be < all(".combined .box-content li").count
+    expect(before).to be < all("section.feeds .box-content li").count
   end
 
   it "administration should require and administrator" do
