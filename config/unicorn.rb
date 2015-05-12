58a4fe907332c4963748c77b40d8294c7415b997
martent <marten@thavenius.se>
2015-05-12 15:39:58 +0200

    Fixed socket name#

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.sitesearch.sock"
+listen "/tmp/unicorn.dashboard.sock"
 worker_processes 7
 timeout 15
 preload_app false
