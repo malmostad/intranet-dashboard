9809e0dc7dd4ec6753d3257e3fa3f67a779b56fa
martent <marten@thavenius.se>
2015-05-12 15:21:10 +0200

    Fixed path for Unicorn app

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :rails_env, :production
 set :stage, :production
 set :branch, "master"
-ask(:password, nil, echo: false)
+ask(:password, 'for app_runner', echo: false)
 server 'srvubuwebhost23.malmo.se', user: 'app_runner', port: 22, password: fetch(:password), roles: %w{web app db}
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/app_runner/sitesearch/current"
+root = "/home/app_runner/dashboard/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
