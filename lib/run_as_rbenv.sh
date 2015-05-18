faf34a2aabc01efac9f869d2743a0c3c7ff1a744
martent <marten@thavenius.se>
2015-05-18 14:22:14 +0200

    Added run_as_rbenv for workers

--- lib/run_as_rbenv.sh
+++ lib/run_as_rbenv.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# Set users rbenv environment and execute a ruby script with optional args
+
 USER=vagrant
 APP_PATH=/vagrant
 PATH=/home/$USER/.rbenv/bin:/home/$USER/.rbenv/shims:$PATH
 RAILS_ENV=development
 
 cd $APP_PATH
-RAILS_ENV=$RAILS_ENV lib/daemons/feed_worker_ctl $1
+RAILS_ENV=$RAILS_ENV $@
