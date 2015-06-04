37816f3a4495558beeb8ec7723810748d434ce6c
martent <marten@thavenius.se>
2015-06-04 15:30:04 +0200

    Merged profile cookie changes from 1.7-stable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,10 @@ class ApplicationController < ActionController::Base
 
     cookies.permanent[myprofile] = {
       value: {
+        # Singular is for GA
+        department:   departments.present? ? departments.first[:homepage_url].gsub('http://komin.malmo.se/', '') : '',
+        workingfield: workingfields.present? ? workingfields.first[:homepage_url].gsub('http://komin.malmo.se/', '') : '',
+        # Plural for masthead w/dropdowns
         departments:   departments,
         workingfields: workingfields,
         ua: Digest::MD5.hexdigest(current_user.id.to_s + Rails.configuration.secret_key_base)
