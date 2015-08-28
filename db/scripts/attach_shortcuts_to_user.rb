7e085285bed871a0e56f715a03350b747c639143
martent <marten@thavenius.se>
2015-08-28 18:58:13 +0200

    Optimized shortcuts script

--- db/scripts/attach_shortcuts_to_user.rb
+++ db/scripts/attach_shortcuts_to_user.rb
@@ -2,14 +2,12 @@ class AttachShortcutsToUser
   # Run once to attach the shortcuts users has through roles directly to user
   # https://github.com/malmostad/intranet-dashboard/issues/61
   def self.migrate
-    User.all.each do |user|
-      user.roles.each do |role|
-        role.shortcuts.each do |shortcut|
-          unless user.shortcuts.include? shortcut
-            user.shortcuts << shortcut
-          end
-        end
+    User.find_each do |user|
+      shortcuts = user.shortcuts
+      user.roles.includes(:shortcuts).each do |role|
+        shortcuts += role.shortcuts
       end
+      user.shortcuts = shortcuts.uniq
     end
   end
 end
