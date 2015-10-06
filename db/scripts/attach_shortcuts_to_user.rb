953065e7a8dbca7ee8895d7c6ad36d9aff5b667c
martent <marten@thavenius.se>
2015-10-06 10:17:29 +0200

    Disabled debugging

--- db/scripts/attach_shortcuts_to_user.rb
+++ db/scripts/attach_shortcuts_to_user.rb
@@ -2,7 +2,7 @@ class AttachShortcutsToUser
   # Run once to attach the shortcuts users has through roles directly to user
   # https://github.com/malmostad/intranet-dashboard/issues/61
   def self.migrate
-    User.limit(5).each do |user|
+    User.find_each do |user|
       shortcuts = user.shortcuts
       user.roles.includes(:shortcuts).each do |role|
         shortcuts += role.shortcuts
