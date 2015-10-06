ac4b06a585c83cd411b6d8bf2232f58eaf4804a5
martent <marten@thavenius.se>
2015-10-06 10:16:52 +0200

    Setting roles selected fields to true in migration script

--- db/scripts/attach_shortcuts_to_user.rb
+++ db/scripts/attach_shortcuts_to_user.rb
@@ -2,7 +2,7 @@ class AttachShortcutsToUser
   # Run once to attach the shortcuts users has through roles directly to user
   # https://github.com/malmostad/intranet-dashboard/issues/61
   def self.migrate
-    User.find_each do |user|
+    User.limit(5).each do |user|
       shortcuts = user.shortcuts
       user.roles.includes(:shortcuts).each do |role|
         shortcuts += role.shortcuts
@@ -10,6 +10,7 @@ class AttachShortcutsToUser
       user.shortcuts = shortcuts.uniq
       user.department_selected = true
       user.working_field_selected = true
+      user.save(validate: false)
     end
   end
 end
