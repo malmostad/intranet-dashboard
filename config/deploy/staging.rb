984dd8c7ca115b9991ca595b0d9f1ba9ce002747
martent <marten@thavenius.se>
2016-12-06 16:10:31 +0100

    Changed branch for deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :rails_env, :test
 set :stage, :test
-set :branch, "master"
+set :branch, "utf8mb"
 ask(:password, 'for app_runner', echo: false)
 server 'srvubuwebtest23.malmo.se', user: 'app_runner', port: 22, password: fetch(:password), roles: %w{web app db}
