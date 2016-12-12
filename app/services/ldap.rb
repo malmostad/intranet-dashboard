51675845252e174a87f7c7f63f820e415e3c174a
martent <marten@thavenius.se>
2016-12-12 17:23:01 +0100

    Exended ldap find_user logging

--- app/services/ldap.rb
+++ app/services/ldap.rb
@@ -38,7 +38,7 @@ class Ldap
     ldap_user = @client.search(base: APP_CONFIG['ldap']['base_dn'], filter: "cn=#{user.username.downcase}",
         attributes: %w(cn givenname sn displayname mail telephonenumber mobile
             title manager extensionattribute1 roomnumber streetaddress
-            company physicalDeliveryOfficeName houseIdentifier division department)).first
+            company physicaldeliveryofficenameeName houseidentifier division department)).first
 
     if ldap_user.present?
       @address = { dashboard: user.address, ldap: ldap_user['streetaddress'].first }
@@ -51,8 +51,8 @@ class Ldap
       user.company                       = ldap_user['company'].first
       user.department                    = ldap_user['division'].first
       user.adm_department                = ldap_user['department'].first
-      user.house_identifier              = ldap_user['houseIdentifier'].first
-      user.physical_delivery_office_name = ldap_user['physicalDeliveryOfficeName'].first
+      user.house_identifier              = ldap_user['houseidentifier'].first
+      user.physical_delivery_office_name = ldap_user['physicaldeliveryofficename'].first
       user.address                       = ldap_user['streetaddress'].first if user.address.blank? # Selective sync
       user.room                          = ldap_user['roomnumber'].first if user.room.blank? # Selective sync
       user.manager                       = User.where(username: extract_cn(ldap_user["manager"].first)).first
@@ -81,22 +81,22 @@ class Ldap
     ldap_user = @client.search(base: APP_CONFIG['ldap']['base_dn'], filter: "cn=#{username}").first
     if ldap_user.present?
       Rails.logger.info ldap_user.inspect
-      puts "ldap_user['givenname']: #{ldap_user['givenname']}"
-      puts "ldap_user['sn']: #{ldap_user['sn']}"
-      puts "ldap_user['displayname']: #{ldap_user['displayname']}"
-      puts "ldap_user['title']: #{ldap_user['title']}"
-      puts "ldap_user['mail']: #{ldap_user['mail']}"
-      puts "ldap_user['company']: #{ldap_user['company']}"
-      puts "ldap_user['division']: #{ldap_user['division']}"
-      puts "ldap_user['department']: #{ldap_user['department']}"
-      puts "ldap_user['houseIdentifier']: #{ldap_user['houseIdentifier']}"
-      puts "ldap_user['physicalDeliveryOffic']: #{ldap_user['physicalDeliveryOffic']}"
-      puts "ldap_user['streetaddress']: #{ldap_user['streetaddress']}"
-      puts "ldap_user['roomnumber']: #{ldap_user['roomnumber']}"
-      puts "ldap_user['telephonenumber']: #{ldap_user['telephonenumber']}"
-      puts "ldap_user['mobile']: #{ldap_user['mobile']}"
-      puts "ldap_user['manager']: #{ldap_user['manager']}"
-      puts "extracted mnager cn: #{extract_cn(ldap_user['manager'].first)}"
+      puts "ldap_user['givenname']: #{ldap_user['givenname'].first}"
+      puts "ldap_user['sn']: #{ldap_user['sn'].first}"
+      puts "ldap_user['displayname']: #{ldap_user['displayname'].first}"
+      puts "ldap_user['title']: #{ldap_user['title'].first}"
+      puts "ldap_user['mail']: #{ldap_user['mail'].first}"
+      puts "ldap_user['company']: #{ldap_user['company'].first}"
+      puts "ldap_user['division']: #{ldap_user['division'].first}"
+      puts "ldap_user['department']: #{ldap_user['department'].first}"
+      puts "ldap_user['houseidentifier']: #{ldap_user['houseidentifier'].first}"
+      puts "ldap_user['physicaldeliveryofficename']: #{ldap_user['physicaldeliveryofficename'].first}"
+      puts "ldap_user['streetaddress']: #{ldap_user['streetaddress'].first}"
+      puts "ldap_user['roomnumber']: #{ldap_user['roomnumber'].first}"
+      puts "ldap_user['telephonenumber']: #{ldap_user['telephonenumber'].first}"
+      puts "ldap_user['mobile']: #{ldap_user['mobile'].first}"
+      puts "ldap_user['manager']: #{ldap_user['manager'].first}"
+      puts "extracted manager cn: #{extract_cn(ldap_user['manager'].first).first}"
     else
       Rails.logger.debug "No user #{username}"
     end
