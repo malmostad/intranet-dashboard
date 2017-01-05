3c41078bb7ed6b309c1effa01042e0978646fd55
martent <marten@thavenius.se>
2017-01-05 16:59:48 +0100

    Unicorn test with preload_app true and resetting of db connections on forking

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,4 +7,23 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.dashboard.sock"
 worker_processes 8
 timeout 15
-preload_app false
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
