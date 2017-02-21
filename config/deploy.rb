abcb5b286251c9411930984d6413495cc904fe72
martent <marten@thavenius.se>
2017-02-21 15:41:36 +0100

    Updated capistrano conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :default_env, { path: '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
 set :keep_releases, 5
 
 namespace :deploy do
-  %w[stop start restart upgrade].each do |command|
+  %w[stop start upgrade].each do |command|
     desc "#{command} unicorn server"
     task command do
       on roles(:all), except: {no_release: true} do
@@ -35,21 +35,34 @@ namespace :deploy do
     end
   end
 
-  desc "Full restart of unicorn server"
-  task :full_restart do
+  desc "Restart of unicorn server"
+  task :restart do
     on roles(:all), except: {no_release: true} do
-      execute "/etc/init.d/unicorn_#{fetch(:application)} restart"
+      # restart/reload isn't enough
+      Rake::Task["deploy:stop"].invoke
+      Rake::Task["deploy:start"].invoke
     end
   end
 
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
+  desc 'Restart daemons'
+  task :restart_daemons do
     on roles(:all) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
-        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
-        puts "Run `git push` to sync changes."
-        exit
-      end
+      puts "Restarting daemons, this can take a while..."
+      ror_ruby = "cd #{release_path} && RAILS_ENV=#{fetch(:rails_env).to_s} #{fetch(:rbenv_prefix)} bundle exec ruby"
+      execute "#{ror_ruby} rake feed_worker:restart[main_feeds]"
+      execute "#{ror_ruby} rake feed_worker:start[user_feeds]"
+      execute "#{ror_ruby} bin/delayed_job restart"
+    end
+  end
+
+  desc 'Perform a backup using mysqldump'
+  task :mysql_backup do
+    on roles(:all) do
+      filepath = "#{fetch(:backup_dir)}predeploy-#{fetch(:release_timestamp)}.sql.bz2"
+      text = capture "cat #{fetch(:shared_path)}/config/database.yml"
+      db_config = YAML::load(text)[fetch(:rails_env).to_s]
+
+      execute "mysqldump --user=#{db_config['username']} --password=#{db_config['password']} #{db_config['database']} | bzip2 -c > #{filepath}"
     end
   end
 
@@ -72,31 +85,21 @@ namespace :deploy do
     end
   end
 
-  desc 'Restart daemons'
-  task :restart_daemons do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
     on roles(:all) do
-      puts "Restarting daemons, this can take a while..."
-      ror_ruby = "cd #{release_path} && RAILS_ENV=#{fetch(:rails_env).to_s} #{fetch(:rbenv_prefix)} bundle exec ruby"
-      execute "#{ror_ruby} lib/daemons/feed_worker_ctl restart"
-      execute "#{ror_ruby} bin/delayed_job restart"
-    end
-  end
-
-  desc 'Perform a backup using mysqldump'
-  task :mysql_backup do
-    on roles(:all) do
-      filepath = "#{fetch(:backup_dir)}predeploy-#{fetch(:release_timestamp)}.sql.bz2"
-      text = capture "cat #{fetch(:shared_path)}/config/database.yml"
-      db_config = YAML::load(text)[fetch(:rails_env).to_s]
-
-      execute "mysqldump --user=#{db_config['username']} --password=#{db_config['password']} #{db_config['database']} | bzip2 -c > #{filepath}"
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts "Run `git push` to sync changes."
+        exit
+      end
     end
   end
 
   before :starting, "deploy:are_you_sure"
   before :starting, "deploy:check_revision"
   before :migrate, "deploy:mysql_backup"
-  after :published, "deploy:full_restart"
-  after :published, "deploy:restart_daemons" # Do it manually if needed with `sudo monit restart [feed_worker|delayed_job]`
+  after :published, "deploy:restart"
+  after :published, "deploy:restart_daemons" # Do it manually if needed with `sudo monit restart [feed_worker_main_feeds|feed_worker_user_feeds/delayed_job]`
   after :finishing, "deploy:cleanup"
 end
