c29b849b681be8b41e73ba47476c4c78b8dd74a4
martent <marten@thavenius.se>
2017-02-27 11:16:12 +0100

    Deploy bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :shared_path, -> { shared_path }
 
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 set :application, 'dashboard'
